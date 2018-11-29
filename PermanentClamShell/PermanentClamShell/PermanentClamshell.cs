3fbafb9e013166b8796e221b419f4812b845b348
severedsolo <severedsolo@users.noreply.github.com>
2018-11-29 19:01:01 +0000

    Initial Commit

---
+++ PermanentClamShell/PermanentClamShell/PermanentClamshell.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace PermanentClamShell
+{
+    public class PermanentClamshell : PartModule
+    {
+        [KSPField(isPersistant = true, guiActive = false)]
+        bool firstSet = false;
+
+        void Start()
+        {
+            Debug.Log("PermanentClamsShell:Start");
+            if (firstSet) return;
+            ModuleProceduralFairing fairing = part.FindModuleImplementing<ModuleProceduralFairing>();
+            fairing.useClamshell = true;
+            firstSet = true;
+            Debug.Log("[PermanentClamshell]: Set Fairing successfully");
+        }
+    }
+}
